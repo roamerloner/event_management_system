9e0f9f6db699fda57b5a8b6bcdab4593c78a9dc1
roamerloner <mkhan181046@bscse.uiu.ac.bd>
2025-01-31 17:45:24 +0600

    Generate report Error fixed. Put some checks in it before generating report.

--- admin/generate_report.php
+++ admin/generate_report.php
@@ -14,17 +14,33 @@ if($event_id === 0){
 
 $conn = getDBConnection();
 $stmt = $conn->prepare("SELECT name FROM events WHERE id = ?");
+if(!$stmt){
+    die("Error preparing statement: ". $conn->error);
+}
 $stmt->bind_param("i", $event_id);
-$stmt->execute();
+if(!$stmt->execute()){
+    die("Error executing statement: " . $tmt->error);
+}
+
 $result = $stmt->get_result();
 
 if($result->num_rows === 0){
     redirectTo('dashboard.php');
 }
 
+
+$event_name = $result->fetch_assoc()['name'];
+$stmt->close();
+
+//fetch attendees name
 $stmt =$conn->prepare("SELECT name, email FROM attendees WHERE event_id = ?");
+if(!$stmt){
+    die("Error preparing statement: ". $conn->error);
+}
 $stmt->bind_param("i", $event_id);
-$stmt->execute();
+if(!$stmt->execute()){
+    die("Error executing statement: " . $tmt->error);
+}
 $result = $stmt->get_result();
 $recent_events = $result->fetch_all(MYSQLI_ASSOC);
 $stmt->close();
@@ -38,8 +54,13 @@ header('Content-Disposition: attachment; filename="' . $filename . '"');
 $output = fopen('php://output', 'w');
 fputcsv($output, array('Name', 'Email'));
 
+
+if(!empty($attendees)){
 foreach($attendees as $attendee){
     fputcsv($output, $attendee);
+}
+} else {
+    fputcsv($output, array("No attendees found for this event."));
 }
 
 fclose($output);
